6f04132d875f309beaad2ff8ce219f9a0fcb96a5
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-09-08 12:13:57 +0200

    dev: added coordonates on small circles

--- main.js
+++ main.js
@@ -6,13 +6,19 @@ const svgns = "http://www.w3.org/2000/svg";
 
 const container = document.querySelector("svg g.samples");
 
+const x0 = 50;
+const y0 = 50;
+const r0 = 45;
+
 const r = 1;
 
 for (let i = 0; i < samples; i++) {
   console.log("i: ", i);
 
-  const x = 34;
-  const y = 56;
+  const angle = i * ((2 * Math.PI) / samples) - Math.PI / 2;
+
+  const x = x0 + r0 * Math.cos(angle);
+  const y = y0 + r0 * Math.sin(angle);
 
   const circle = document.createElementNS(svgns, "circle");
   circle.setAttributeNS(null, "cx", x);
