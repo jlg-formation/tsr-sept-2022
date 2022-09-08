1b914d49629c701a08c3feca56f296dd2a0f5401
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-09-08 15:30:35 +0200

    refactor: creating functions with math

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
 console.log("start");
 
+const computeAngle = (i, samples, multiplicationFactor) => {
+  if (multiplicationFactor === undefined) {
+    multiplicationFactor = 1;
+  }
+  return i * ((2 * Math.PI) / samples) * multiplicationFactor - Math.PI / 2;
+};
+
+const computePointCoordinate = (angle) => {
+  const x = x0 + r0 * Math.cos(angle);
+  const y = y0 + r0 * Math.sin(angle);
+  return { x: x, y: y };
+};
+
 const samples = 100;
 const multiplicationFactor = 3;
 
@@ -17,34 +30,30 @@ const r = 1;
 for (let i = 0; i < samples; i++) {
   console.log("i: ", i);
 
-  const angle = i * ((2 * Math.PI) / samples) - Math.PI / 2;
+  const angle = computeAngle(i, samples);
+  const { x, y } = computePointCoordinate(angle);
 
-  const x = x0 + r0 * Math.cos(angle);
-  const y = y0 + r0 * Math.sin(angle);
-
-  const circle = document.createElementNS(svgns, "circle");
-  circle.setAttributeNS(null, "cx", x);
-  circle.setAttributeNS(null, "cy", y);
-  circle.setAttributeNS(null, "r", r);
-  container.appendChild(circle);
+  const sample = document.createElementNS(svgns, "circle");
+  sample.setAttributeNS(null, "cx", x);
+  sample.setAttributeNS(null, "cy", y);
+  sample.setAttributeNS(null, "r", r);
+  container.appendChild(sample);
 }
 
-const drawLine = (x1, y1, x2, y2) => {
+const drawLine = (p1, p2) => {
   const line = document.createElementNS(svgns, "line");
-  line.setAttributeNS(null, "x1", x1);
-  line.setAttributeNS(null, "y1", y1);
-  line.setAttributeNS(null, "x2", x2);
-  line.setAttributeNS(null, "y2", y2);
+  line.setAttributeNS(null, "x1", p1.x);
+  line.setAttributeNS(null, "y1", p1.y);
+  line.setAttributeNS(null, "x2", p2.x);
+  line.setAttributeNS(null, "y2", p2.y);
   lineContainer.appendChild(line);
 };
 
 for (let i = 0; i < samples; i++) {
-  const angle1 = i * ((2 * Math.PI) / samples) - Math.PI / 2;
-  const angle2 =
-    i * ((2 * Math.PI) / samples) * multiplicationFactor - Math.PI / 2;
-  const x1 = x0 + r0 * Math.cos(angle1);
-  const y1 = y0 + r0 * Math.sin(angle1);
-  const x2 = x0 + r0 * Math.cos(angle2);
-  const y2 = y0 + r0 * Math.sin(angle2);
-  drawLine(x1, y1, x2, y2);
+  // draw a line between i and i * multiplicationFactor
+  const angle1 = computeAngle(i, samples);
+  const p1 = computePointCoordinate(angle1);
+  const angle2 = computeAngle(i, samples, multiplicationFactor);
+  const p2 = computePointCoordinate(angle2);
+  drawLine(p1, p2);
 }
